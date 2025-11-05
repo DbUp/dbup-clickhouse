f21eb5d1f9b6da3ab6f7ca00aa7eb21d04414b73
Robert Wagner <robert@wagner.id.au>
2025-11-05 16:09:00 +1000

    Add XML documentation for all public members

--- src/dbup-clickhouse/ClickHouseExtensions.cs
+++ src/dbup-clickhouse/ClickHouseExtensions.cs
@@ -3,8 +3,19 @@ using DbUp.Builder;
 
 namespace DbUp.ClickHouse;
 
+/// <summary>
+/// Configuration extension methods for ClickHouse.
+/// </summary>
 public static class ClickHouseExtensions
 {
+    /// <summary>
+    /// Creates an upgrader for ClickHouse databases.
+    /// </summary>
+    /// <param name="supportedDatabases">Fluent helper type.</param>
+    /// <param name="connectionString">ClickHouse database connection string.</param>
+    /// <returns>
+    /// A builder for a database upgrader designed for ClickHouse databases.
+    /// </returns>
     public static UpgradeEngineBuilder ClickHouseDatabase(this SupportedDatabases supportedDatabases, string connectionString)
     {
         throw new NotImplementedException();
--- src/dbup-clickhouse/ClickHouseJournal.cs
+++ src/dbup-clickhouse/ClickHouseJournal.cs
@@ -6,6 +6,15 @@ using DbUp.Engine.Transactions;
 
 namespace DbUp.ClickHouse;
 
+/// <summary>
+/// An implementation of the <see cref="IJournal"/> interface for ClickHouse databases.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="ClickHouseJournal"/> class.
+/// </remarks>
+/// <param name="connectionManagerFactory">The connection manager factory.</param>
+/// <param name="logFactory">The log factory.</param>
+/// <param name="tableName">The name of the table.</param>
 public class ClickHouseJournal(
 // Remove pragma once implemented
 #pragma warning disable CS9113 // Parameter is unread.
@@ -15,9 +24,12 @@ public class ClickHouseJournal(
 #pragma warning restore CS9113 // Parameter is unread.
     ) : IJournal
 {
+    /// <inheritdoc/>
     public string[] GetExecutedScripts() => throw new NotImplementedException();
 
+    /// <inheritdoc/>
     public void StoreExecutedScript(SqlScript script, Func<IDbCommand> dbCommandFactory) => throw new NotImplementedException();
 
+    /// <inheritdoc/>
     public void EnsureTableExistsAndIsLatestVersion(Func<IDbCommand> dbCommandFactory) => throw new NotImplementedException();
 }
